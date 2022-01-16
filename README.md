90d4b1e833fd74a18a7b4b30ce4e0ccfa3e83cca
rummanrc <rumman179@gmail.com>
2022-01-16 16:49:02 +0900

    fresh start using API configs this time

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ application up and running.
 
 Things you may want to cover:
 
-* Ruby version 2.7
+* Ruby version
 
 * System dependencies
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  root "articles#index"
-  resources :articles
-
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
