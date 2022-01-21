89dff0bc72e612ae1f4b7edcd4759fedc08c42f7
rumman <rumman@co-well.jp>
2022-01-21 19:55:39 +0900

    added jwt auth and login controller (WIP)

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ ruby "2.7.0"
 gem 'bcrypt', '~> 3.1.7'
 gem 'rack-cors'
 gem 'jwt'
+gem "dotenv-rails", groups: [:development, :test]
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.0"
---
+++ app/controllers/login_controller.rb
@@ -0,0 +1,15 @@
+class LoginController < ApplicationController
+  def create
+    user = User.find_by("lower(email) = ?", user_params[:email].downcase)
+    if user && user.authenticate(user_params[:password])
+      render json: { token: token(user.id), user_id: user.id }, status: :created
+    else
+      render json: { errors: [ "Sorry, incorrect email or password" ] }, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+end
